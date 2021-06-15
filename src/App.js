12460951d1f87bb28bb951cb7153b1eda0d31755
Paul <pauldavis1826@gmail.com>
2021-06-15 12:41:33 -0700

    trying to get a scratch single page app off the ground

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            drillDown: 'Home'
+        }
+        this.render(
+            <div>
+                <H1>Paul Robert Davis</H1>
+            </div>
+        )
+    }
+}
+
+export default App
